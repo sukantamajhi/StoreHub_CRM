f91b55656c09e03691693f915a9e72f292a3c573
Sukanta Majhi <sukantamajhi799@gmail.com>
2024-12-23 07:25:33 +0530

    Changes done

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 					<li className="mb-2">
 						Get started by editing{' '}
 						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-							pages/index.tsx
+							Hello world
 						</code>
 						.
 					</li>
--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello world from server');
+	res.send('Hello world from server!!!');
 });
 
 app.listen(5000, () => {
