d1d66b076387306b3299b6d3eed15e7b055e0a78
Sukanta Majhi <sukantamajhi799@gmail.com>
2024-12-23 07:33:16 +0530

    prepush changes checking

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,17 @@ import express from 'express';
 
 const app = express();
 
+
+
+
+
 app.get('/', (req, res) => {
 	res.send('Hello world from server!!!');
 });
+
+
+
+
 
 app.listen(5000, () => {
 	console.log('Running on port 5000');
