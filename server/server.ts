89964acb91309f5d71bc27eb85411a31eaf181f0
Sukanta Majhi <sukantamajhi799@gmail.com>
2024-12-23 07:34:20 +0530

    Changes done

--- server/server.ts
+++ server/server.ts
@@ -2,17 +2,9 @@ import express from 'express';
 
 const app = express();
 
-
-
-
-
 app.get('/', (req, res) => {
 	res.send('Hello world from server!!!');
 });
-
-
-
-
 
 app.listen(5000, () => {
 	console.log('Running on port 5000');
