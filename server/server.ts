9fae7bbec5315cfd2f3d509d08311e4ed5e04ff8
Sukanta Majhi <71279597+sukantamajhi@users.noreply.github.com>
2024-12-24 14:22:58 +0000

    Changes done

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose
+	.connect(process.env.MONGO_URI as string)
+	.then(() => {
+		console.log('Database connected successfully');
+	})
+	.catch((err) => {
+		console.error(err, '<<-- Error in connecting with database');
+	});
 
 app.get('/', (req, res) => {
 	res.send('Hello world from server');
